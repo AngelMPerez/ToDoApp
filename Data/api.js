1606677bff76775566034ff23e8b6ddf3001313f
Angel Perez <angelmperezcarranza@gmail.com>
2019-12-26 18:28:27 -0700

    new api

---
+++ Data/api.js
@@ -0,0 +1,40 @@
+const Express =require('express')
+const server = Express()
+const app = require('../data/database')
+const cors = require('cors')
+server.use(Express.json())
+server.use(cors())
+
+server.listen(4000,async ()=>{
+    // console.log(await app.connection())
+    console.log("Ready in Port 4000")
+})
+
+server.get('/',async (req, res) => {
+    // const results = app.read()
+    
+    res.send(await app.read())
+})
+
+server.get('/:type',async (req, res) => {
+    // const results = app.read()
+    res.send(await app.readFilter(req.params.type))
+    console.log(req.params.type)
+})
+
+server.post('/',async function(req,res){
+    await app.create(req.body)
+    console.log(req.body)
+    res.send('success')
+})
+
+server.patch('/:_id',async function(req,res){
+    res.send(await app.update(req.body,req.params._id))
+})
+
+server.delete('/:_id',async function(req,res){
+    let result = await app.erase(req.params._id)
+    // console.log(req.params)
+    console.log(result)
+    res.send('removed : '+ req.params._id )
+})
